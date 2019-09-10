30d115008db5bc6f1be3577c3911cc8dbb10e8b0
IBUNHABIBU <ibunhabib@gmail.com>
2019-09-10 03:51:36 +0300

    testing my_any?

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -18,14 +18,24 @@ RSpec.describe Enumerable do
 
     describe "#my_select" do
         it "it takes all element that meet the condition" do
-            result=[1,2,3,8,7,10,13].my_select { |item| item>8}
+            result=[1,2,3,8,7,10,13].my_select { |item| item>8 }
             expect(result).to eql([10,13])
         end
     end
 
     describe "#my_all?" do
         it "returns true if all element are true otherwise false" do
-            expect([2,4,8].my_all? {|item| item.even? }).to eql(true)
+            expect([2,4,8].my_all? { |item| item.even? }).to eql(true)
         end
     end
+
+    describe "#my_any?" do
+        it "return true if any of the element is true and false if all element are false" do
+            expect([2,5,7,9].my_any? { |item| item.even? }).to eql(true)
+        end
+    end
+
+    describe "#my_none?" do
+        it ""
+        
 end
